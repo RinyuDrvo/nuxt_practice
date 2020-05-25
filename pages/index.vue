a7ddfa17dd996609b628b613c1d0c5662cf6d575
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-05-25 11:46:17 +0900

    make index

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,38 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt_practice
-      </h1>
-      <h2 class="subtitle">
-        My outstanding Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div id="app">
+    <h1 class="title">
+      アプリ名
+    </h1>
+    <input v-model="newItem" type="text">
+    <form @submit.prevent>
+      <button @click="addItem">
+        add
+      </button>
+    </form>
+    <li v-for="todo in todos" :key="todo.item">
+      {{ todo.item }}
+    </li>
+    <h2 class="subtitle">
+      アプリ名
+    </h2>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data () {
+    return {
+      newItem: '',
+      todos: []
+    }
+  },
+  mothods: {
+    addItem (event) {
+      const todo = {
+        item: this.newItem
+      }
+      this.todos.push(todo)
+    }
   }
 }
 </script>
